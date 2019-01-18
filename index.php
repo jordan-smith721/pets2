8d82802fc94faadacfd36a44312a624e20f50106
Ean Daus <eandaus123@gmail.com>
2019-01-18 13:24:57 -0500

    Added a route that makes animal noises

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Jsmit
+ * User: Jsmit and Ean Daus
  * Date: 1/18/2019
  * Time: 10:06 AM
  */
@@ -23,6 +23,32 @@ $f3->set('DEBUG', 3);
 $f3->route('GET /', function() {
     echo"<h1>My Pets</h1>";
     echo"<a href='order'>Order a Pet</a>";
+});
+
+//Define a parameterized route
+$f3->route('GET /@animal', function($f3, $params) {
+    $animal = $params['animal'];
+    $noise = '';
+    if($animal == 'dog')
+    {
+        $noise = "Woof";
+    }elseif($animal == 'chicken')
+    {
+        $noise = "Cluck";
+    }elseif($animal == 'cow')
+    {
+        $noise = "Moo";
+    }elseif($animal == 'cat')
+    {
+        $noise = "Meow";
+    }elseif($animal == 'pig')
+    {
+        $noise = "Oink";
+    }
+    else {
+        $f3->error(404);
+    }
+    echo"<h1>$noise!</h1>";
 });
 
 //Run fat free
